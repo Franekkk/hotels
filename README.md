f3582cb21f1cd6b6cfd8da17a57b991d27de9137
Piotr Rozmarynowski <venstd@gmail.com>
2018-12-16 02:47:17 +0100

    add first-run.sh script

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Dokumentacja
 
-## Instalacja
-1. Wymagana inicjalizacja projektu z osobna
+## Pierwsze uruchomienie
+1. `docker-compose up -d` i poczekać aż zbuduje się wszystko.
+2. `./first-run.sh` zainicjuje zależności na wszystkich serwisach dockerowych które tego potrzebują.
 
 
 ## Testy
--- first-run.sh
+++ first-run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-docker-compose up -d hotel_krakow
-
 init_service() {
     docker-compose exec $1 php init;
+    docker-compose exec $1 composer update;
     docker-compose exec $1 composer install;
 }
 
+# there is no need to run init_service for other hotels as they share the same code
 init_service hotel_krakow
-init_service hotel_wroclaw
-init_service hotel_gdansk
 
-docker-compose up -d
+init_service hotel_worker
+init_service hotel_agent
