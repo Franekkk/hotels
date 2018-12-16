1027835ecfff952c8258ce91663838026e2a91f7
Piotr Rozmarynowski <venstd@gmail.com>
2018-12-16 03:04:26 +0100

    docs

--- first-run.sh
+++ first-run.sh
@@ -9,5 +9,5 @@ init_service() {
 # there is no need to run init_service for other hotels as they share the same code
 init_service hotel_krakow
 
-init_service hotel_worker
-init_service hotel_agent
+init_service worker
+init_service agent
